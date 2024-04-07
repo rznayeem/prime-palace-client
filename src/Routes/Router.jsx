921a7e46e66b8815b0d739cd6d7f74ff6d3b201e
rznayeem <riyazulnayeem@gmail.com>
2024-04-07 23:51:33 +0600

    initial files created

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Home from '../pages/Home/Home';
+import Root from '../Layouts/Root';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root></Root>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
+
+export default router;
